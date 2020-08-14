6cbf2c2463263ab8c14f7f823f3beb1f00c32f0e
Chrissie Muhorakeye <chrissiemhrk@gmail.com>
2020-08-14 13:33:05 +0200

    style: remove default styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,28 +4,4 @@
   </div>
 </template>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-html {
-  font-family: 'Open Sans', sans-serif;
-  font-size: 0.8rem;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-}
-</style>
+<style></style>
