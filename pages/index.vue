03ced0933f629860486140bcedca42b379aeedbe
Chrissie Muhorakeye <chrissiemhrk@gmail.com>
2020-08-14 14:29:22 +0200

    refactor: remove button class from nuxt-link tag

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,13 @@
       <div class="links pt-5 flex flex-col md:flex-row">
         <nuxt-link
           to="/two-color-gradient"
-          class="button--green inline-block border-2 border-primary-900 py-2 px-5 mb-5 rounded-md md:mr-10"
+          class="inline-block border-2 border-primary-900 py-2 px-5 mb-5 rounded-md md:mr-10"
         >
           Two color gradient
         </nuxt-link>
         <nuxt-link
           to="/three-color-gradient"
-          class="button--grey inline-block border-2 border-primary-900 py-2 px-5 mb-5 rounded-md"
+          class="inline-block border-2 border-primary-900 py-2 px-5 mb-5 rounded-md"
         >
           Three color gradient
         </nuxt-link>
