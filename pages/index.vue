77b85df26e8c9fcb86ea23d23340c0a0467081e3
Chrissie Muhorakeye <chrissiemhrk@gmail.com>
2020-08-14 14:09:03 +0200

    style: add responsive styling

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       <div class="links pt-5 flex flex-col md:flex-row">
         <nuxt-link
           to="/two-color-gradient"
-          class="button--green inline-block border-2 border-primary-900 py-2 px-5 mb-5 rounded-md md:mr-5"
+          class="button--green inline-block border-2 border-primary-900 py-2 px-5 mb-5 rounded-md md:mr-10"
         >
           Two color gradient
         </nuxt-link>
